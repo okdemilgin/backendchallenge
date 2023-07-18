2d9a35d269eaf0c52927af42fcac1cf4fddff4fe
ilginokdem <okdemilgin@gmail.com>
2023-07-18 19:57:18 +0300

    backend security

--- api/server.js
+++ api/server.js
@@ -3,9 +3,15 @@
 const express = require('express');
 const server = express();
 require ('dotenv').config();
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
 
 //global middleware
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use(morgan("dev"));
 
 //router
 
